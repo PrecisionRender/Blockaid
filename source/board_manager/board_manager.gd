dde0bab6b999f9ff821cbda0f05696b5f199ab60
PrecisionRender <austin08dean@gmail.com>
2023-09-11 21:59:48 -0500

    Fix embed subwindows functionality

--- source/board_manager/board_manager.gd
+++ source/board_manager/board_manager.gd
@@ -63,6 +63,7 @@ func _drop_data_fw(at_position: Vector2, data: Variant) -> void:
 
 	SessionInfo.move_current_board_to(board_list.get_selected().get_index())
 
+
 func _update_session_title() -> void:
 	session_title_edit.hide()
 	session_title_label.show()
@@ -74,9 +75,7 @@ func _update_session_title() -> void:
 
 
 func _on_board_list_item_selected() -> void:
-	if SessionInfo.boards.size() - 1 < board_list.get_selected().get_index():
-		return
-	SessionInfo.set_current_board_index(board_list.get_selected().get_index())
+	pass
 
 
 func _on_tree_item_activated() -> void:
@@ -112,3 +111,59 @@ func _on_add_board_button_pressed() -> void:
 	item.custom_minimum_height = 25
 	board_list.set_selected(item, 0)
 	SessionInfo.create_new_board()
+
+
+func _on_board_list_item_mouse_selected(position: Vector2, mouse_button_index: int) -> void:
+	if SessionInfo.boards.size() - 1 < board_list.get_selected().get_index():
+		return
+	SessionInfo.set_current_board_index(board_list.get_selected().get_index())
+
+	if mouse_button_index != MOUSE_BUTTON_MASK_RIGHT:
+		return
+
+	var context_menu: PopupMenu = PopupMenu.new()
+	context_menu.initial_position = Window.WINDOW_INITIAL_POSITION_ABSOLUTE
+	
+	context_menu.position = get_screen_position() + get_viewport().get_mouse_position()
+	context_menu.min_size.x = 250
+
+	context_menu.add_item("Cut")
+	context_menu.add_item("Copy")
+	context_menu.add_item("Paste")
+	context_menu.add_separator()
+	context_menu.add_item("Delete")
+
+	context_menu.set_item_disabled(0, true)
+	context_menu.set_item_disabled(1, true)
+	context_menu.set_item_disabled(2, true)
+	context_menu.set_item_disabled(4, true)
+	context_menu.set_item_tooltip(0, "Not implemented yet")
+	context_menu.set_item_tooltip(1, "Not implemented yet")
+	context_menu.set_item_tooltip(2, "Not implemented yet")
+	context_menu.set_item_tooltip(4, "Not implemented yet")
+
+	var cut_event: InputEventKey = InputEventKey.new()
+	cut_event.keycode = KEY_X | KEY_MASK_CMD_OR_CTRL
+	var copy_event: InputEventKey = InputEventKey.new()
+	copy_event.keycode = KEY_C | KEY_MASK_CMD_OR_CTRL
+	var paste_event: InputEventKey = InputEventKey.new()
+	paste_event.keycode = KEY_V | KEY_MASK_CMD_OR_CTRL
+	var delete_event: InputEventKey = InputEventKey.new()
+	delete_event.keycode = KEY_DELETE
+
+	var cut_shortcut = Shortcut.new()
+	cut_shortcut.events.append(cut_event)
+	var copy_shortcut = Shortcut.new()
+	copy_shortcut.events.append(copy_event)
+	var paste_shortcut = Shortcut.new()
+	paste_shortcut.events.append(paste_event)
+	var delete_shortcut = Shortcut.new()
+
+	delete_shortcut.events.append(delete_event)
+	context_menu.set_item_shortcut(0, cut_shortcut)
+	context_menu.set_item_shortcut(1, copy_shortcut)
+	context_menu.set_item_shortcut(2, paste_shortcut)
+	context_menu.set_item_shortcut(4, delete_shortcut)
+
+	add_child(context_menu)
+	context_menu.show()
